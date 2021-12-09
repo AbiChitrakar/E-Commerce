e47b1c6285f37fea4492566e2fb41bedba3d7c24
9813055493 <abichitrakar2921@gmail.com>
2021-12-09 16:50:46 +0545

    My first commit

---
+++ E-commerce LAB/index.js
@@ -0,0 +1,18 @@
+let showDetails = false;
+function readMore() {
+if (!showDetails) {
+document.getElementById("sidebar__card").innerHTML = `!! Dashain Bada Dhamaka !!
+Discount on each & every items .
+The time to grab the product you always wanted to .  
+The big festive offers you don't want to miss out`;
+document.getElementById("details").innerHTML = "Hide Details";
+}
+else {
+document.getElementById("sidebar__card").innerHTML = `STEAL THE DEAL!`;
+document.getElementById("details").innerHTML = "Read More";
+}
+showDetails = !showDetails;
+}
+function toggleForm(){
+    document.body.classList.toggle('activeForm');
+}
